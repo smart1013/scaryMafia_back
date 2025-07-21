13cf0e40db14662b7c8e8f69162252d1568303fe
boyeonp <dnflwlq20020416@gmail.com>
2025-07-21 14:42:21 +0900

    change for frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORS(Cross-Origin Resource Sharing) 설정 추가 
+  app.enableCors({  
+    origin: 'http://localhost:3000', // 모든 도메인 허용
+    credentials: true, // 쿠키를 포함한 요청 허용
+  });
+  
+  //app.useGlobalPipes(new ValidationPipe());
+
+  // 백엔드 서버 8000번 포트로 실행 
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
